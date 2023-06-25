c5d4853821214cf115dfed793983001c55b9902c
aameerk <aameerk917@gmail.com>
2023-06-25 17:13:31 +0530

    commit -demo

--- Home.py
+++ Home.py
@@ -1,4 +1,4 @@
-import streamlit as st
+
 import leafmap.foliumap as leafmap
 
 st.set_page_config(layout="wide")
@@ -23,18 +23,3 @@ st.markdown(
     """
 )
 
-st.header("Instructions")
-
-markdown = """
-1. For the [GitHub repository](https://github.com/giswqs/streamlit-multipage-template) or [use it as a template](https://github.com/giswqs/streamlit-multipage-template/generate) for your own project.
-2. Customize the sidebar by changing the sidebar text and logo in each Python files.
-3. Find your favorite emoji from https://emojipedia.org.
-4. Add a new app to the `pages/` directory with an emoji in the file name, e.g., `1_🚀_Chart.py`.
-
-"""
-
-st.markdown(markdown)
-
-m = leafmap.Map(minimap_control=True)
-m.add_basemap("OpenTopoMap")
-m.to_streamlit(height=500)
